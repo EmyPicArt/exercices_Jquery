66627a88abb67ee7af40df67b18bee3d3ed505d9
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-17 14:21:39 +0100

    fin ex 8 btn .hide() .show()

--- ex_8_jq/assets/js/script.js
+++ ex_8_jq/assets/js/script.js
@@ -1,9 +1,14 @@
 // vérifier le chargement du Dom avec cette fonction :
 $(function(){
 
-
-
-
+    // cibler le button masquer et au clic function cacher le texte .hide()
+$("#red").click(function(){
+    $("#text").hide();
+})
+    // cibler le button afficher et au clic function montrer le texte .show()
+$("#green").click(function(){
+    $("#text").show();
+})
 
     
 });
