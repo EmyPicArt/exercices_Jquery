8c4b10c8ce257316660eef93f846b0f18db0ad9c
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-17 14:35:22 +0100

    fin ex 9. Click .ccs(color)

--- ex_9_jq/assets/js/script.js
+++ ex_9_jq/assets/js/script.js
@@ -2,8 +2,18 @@
 $(function(){
 
 
+    // cibler le button, evt au clic(ouvrir une fonction : cibler le texte et modifier le .css)
+$("#red").click(function(){
+    $("#text").css('color','red');
+})
 
+$("#blue").click(function(){
+    $("#text").css('color','blue');
+})
 
+$("#green").click(function(){
+    $("#text").css('color','green');
+})
 
     
 });
