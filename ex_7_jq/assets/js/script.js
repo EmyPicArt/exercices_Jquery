c64eba5511221a84a3ca56ed76428c0525d30fd3
EmyPicArt <93980104+EmyPicArt@users.noreply.github.com>
2021-11-17 14:14:50 +0100

    fin ex 7.click function alert()

--- ex_7_jq/assets/js/script.js
+++ ex_7_jq/assets/js/script.js
@@ -1,9 +1,10 @@
 // vérifier le chargement du Dom avec cette fonction :
 $(function(){
 
-
-
-
+    // cibler le button, événement click,ajouter une fonction à l'intérieur une alert
+$("#button").click(function(){
+    alert("Désolé il n'y rien à voir ici")
+})
 
     
 });
